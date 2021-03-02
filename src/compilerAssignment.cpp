1c62ea371917e393a4bc95d6177912652595815a
Kyle Welsh <kswelsh@noctrl.edu>
2021-03-01 18:20:41 -0600

    enhanced file input/read

--- src/compilerAssignment.cpp
+++ src/compilerAssignment.cpp
@@ -15,9 +15,9 @@ using namespace std;
 class LexAnalyzer
 {
 private:
-	vector<string> lexemes;  		// source code file lexemes
-	vector<string> tokens;   		// source code file tokens
-	map<string, string> tokenmap;   // valid lexeme/token pairs
+	vector<string> _lexemes;  		// source code file lexemes
+	vector<string> _tokens;   		// source code file tokens
+	map<string, string> _tokenmap;   // valid lexeme/token pairs
 
 	bool testIfSymbol(char sCandidate)
 	{
@@ -42,6 +42,10 @@ private:
 		{
 			symbol = true;
 		}
+		else if (sCandidate == '!')
+		{
+			symbol = true;
+		}
 
 		return symbol;
 	}
@@ -61,175 +65,241 @@ private:
 		return alpha;
 	}
 
-	void pushString(vector<string>& sourceCode, string &line, int &i, int& j, bool& error)
+	void ifSymbolVariant (string &symbolCandidate, string &line, int &i, bool &symbolFinished, string equalTo)
+	{
+		symbolFinished = true;
+		if (symbolCandidate + line[i] == equalTo)
+		{
+			symbolCandidate.push_back(line[i]);
+			i++;
+		}
+	}
+
+	void checkSymbolVariation(string &symbolCandidate, string &line, int &i, bool &symbolFinished)
+	{
+		if (symbolCandidate >= "(" && symbolCandidate <= "-")
+		{
+			symbolFinished = true;
+		}
+		else if (symbolCandidate == ";")
+		{
+			symbolFinished = true;
+		}
+		else if (symbolCandidate == "=")
+		{
+			ifSymbolVariant(symbolCandidate, line, i, symbolFinished, "==");
+		}
+		else if (symbolCandidate == "<")
+		{
+			ifSymbolVariant(symbolCandidate, line, i, symbolFinished, "<=");
+		}
+		else if (symbolCandidate == ">")
+		{
+			ifSymbolVariant(symbolCandidate, line, i, symbolFinished, ">=");
+		}
+		else if (symbolCandidate == "!")
+		{
+			ifSymbolVariant(symbolCandidate, line, i, symbolFinished, "!=");
+		}
+		else if (symbolCandidate == "&")
+		{
+			ifSymbolVariant(symbolCandidate, line, i, symbolFinished, "&&");
+		}
+		else if (symbolCandidate == "|")
+		{
+			ifSymbolVariant(symbolCandidate, line, i, symbolFinished, "||");
+		}
+	}
+
+	void pushString(string &line, int &i, bool &error, istream &infile)
 	{
 		string stringCandidate = "";
 		bool isString = true;
-
-		while (isString == true && error == false)
+		i++;
+		while (error == false && !infile.eof() && line[i] != 34)
 		{
-			stringCandidate.push_back(line[j]);
-			j++;
+			stringCandidate.push_back(line[i]);
+			i++;
 
-				if (line[j] == 34)
-				{
-					isString = false;
-					stringCandidate.push_back(line[j]);
-					j++;
-				}
-				else if (i == sourceCode.size())
-				{
-					error = true;
-				}
-				else if (j == line.size())
-				{
-					i++;
-					line = sourceCode[i];
-					j = 0;
-					stringCandidate.push_back(' ');
-				}
+			if (i >= line.size())
+			{
+				i = 0;
+				getline(infile, line);
 			}
-		cout << stringCandidate << endl;
+		}
+
+		if (infile.eof() && line[i] != 34)
+		{
+			error = true;
+		}
+		else
+		{
+			_lexemes.push_back(stringCandidate);
+			_tokens.push_back("s_str");
+			i++;
+		}
 	}
 
-	void pushNum()
+	void pushNum(string &line, int &i, bool &error)
 	{
+		string numCandidate = "";
+		while (line[i] >= '0' && line[i] <= '9' && error == false && i <= line.size())
+		{
+			numCandidate.push_back(line[i]);
+			i++;
 
+			if (testIfAlpha(line[i]) && i < line.size())
+			{
+				error = true;
+			}
+		}
+
+		if (error == false)
+		{
+			_lexemes.push_back(numCandidate);
+			_tokens.push_back("t_int");
+		}
 	}
 
-	void pushAlpha(string &line, int &j)
+	void pushAlpha(string &line, int &i)
 	{
 		string alphaCandidate = "";
-		while (testIfAlpha(line[j]))
+		int exist;
+		while (testIfAlpha(line[i]) && i <= line.size())
 		{
-			alphaCandidate.push_back(line[j]);
-			j++;
+			alphaCandidate.push_back(line[i]);
+			i++;
 		}
-		cout << alphaCandidate << endl;
+
+		exist = _tokenmap.count(alphaCandidate);
+		if (exist > 0)
+		{
+			_lexemes.push_back(alphaCandidate);
+			_tokens.push_back(_tokenmap[alphaCandidate]);
+		}
+		else
+		{
+			_lexemes.push_back(alphaCandidate);
+			_tokens.push_back("t_id");
+		}
 	}
 
-	void pushSymbol(string &line, int &j)
+	void pushSymbol(string &line, int &i)
 	{
 		string symbolCandidate = "";
-		while (testIfSymbol(line[j]))
+		bool symbolFinished = false;
+		while (testIfSymbol(line[i]) && i <= line.size() && symbolFinished == false)
 		{
-			symbolCandidate.push_back(line[j]);
-			j++;
+			symbolCandidate.push_back(line[i]);
+			i++;
+			checkSymbolVariation(symbolCandidate, line,i, symbolFinished);
 		}
-		cout << symbolCandidate << endl;
+
+		_lexemes.push_back(symbolCandidate);
+		_tokens.push_back(_tokenmap[symbolCandidate]);
 	}
 
 public:
 	LexAnalyzer(istream& infile)
-	// pre: parameter refers to open data file consisting of token and
-	// lexeme pairs i.e.  s_and and t_begin begin t_int 27.  Each pair    // appears on its own input line.
+	// pre: parameter refers to open data file consisting of token and lexeme pairs i.e.
+	// s_and and t_begin begin t_int 27.  Each pair appears on its own input line.
 	// post: tokenmap has been populated
 	{
-		string lexemInput;
+		string lexemeInput;
 		string tokenInput;
-		string fullInput;	//npt needed
 		if (!infile)
 		{
 			cout << "ERROR: Failed opening file! Exiting program." << endl;
 			exit(-1);
 		}
 
-		infile >> lexemInput >> tokenInput;
+		infile >> tokenInput >> lexemeInput;
 		while (!infile.eof())
 		{
-			tokenmap.insert(make_pair(lexemInput, tokenInput));
-			infile >> lexemInput >> tokenInput;
-
+			_tokenmap.insert(make_pair(lexemeInput, tokenInput));
+			infile >> tokenInput >> lexemeInput;
 		}
 
-		//map<string, string>::iterator it;
-		//for(it = tokenmap.begin(); it != tokenmap.end(); ++it)
+		//map<string, string>::iterator mitr;
+		//for (mitr = _tokenmap.begin(); mitr != _tokenmap.end(); mitr++)
 		//{
-		//  cout << it->first << " : " << it->second << '\n';
+		//	cout << mitr->first << " : " << mitr->second << endl;
 		//}
-		//cout << endl;
 	}
 
 	void scanFile(istream& infile, ostream& outfile)
 	// pre: 1st parameter refers to an open text file that contains source
-	// code in the language, 2nd parameter refers to an open empty output
-	// file
+	// code in the language, 2nd parameter refers to an open empty output file
 	// post: If no error, the token and lexeme pairs for the given input
 	// file have been written to the output file.  If there is an error,
-	// the incomplete token/lexeme pairs, as well as an error message have // written to the output file.  A success or fail message has printed // to the console.
+	// the incomplete token/lexeme pairs, as well as an error message have
+	// written to the output file.  A success or fail message has printed
+	// to the console.
 	{
-		vector<string> sourceCode;
 		string inputLine;
 		bool error = false;
+		int i = 0;
 
 		if (!infile)
 		{
 			cout << "ERROR: Failed opening file! Exiting program." << endl;
 			exit(-1);
 		}
-		infile >> inputLine;
-		while (!infile.eof())
+
+		getline(infile, inputLine);
+		while (!infile.eof() && error == false)
 		{
-			sourceCode.push_back(inputLine);
-			infile >> inputLine;
-		}
 
-		//for (int i = 0; i < sourceCode.size(); i++)
-		//{
-		//	cout << sourceCode[i] << endl;
-		//}
-
-		int i = 0;
-		int j = 0;
-		while (i < sourceCode.size() && error != true)
-		{
-			string lineCandidate = sourceCode[i];
-
-			// ALPHA
-			if (testIfAlpha(lineCandidate[j]))
+			if (testIfAlpha(inputLine[i]))
 			{
-				pushAlpha(lineCandidate, j);
+				pushAlpha(inputLine, i);
 			}
-
-			// NUMBER
-			else if (lineCandidate[j] >= '0' && lineCandidate[j] <= '9')
+			else if (inputLine[i] >= '0' && inputLine[i] <= '9')
 			{
-				cout << "num" << endl;
+				pushNum(inputLine, i, error);
 			}
-
-			// STRING
-			else if (lineCandidate[j] == 34)
+			// ASCII value of " is 34
+			else if (inputLine[i] == 34)
 			{
-				pushString(sourceCode, lineCandidate, i, j, error);
+				pushString(inputLine, i, error, infile);
 			}
-
-			// SYMBOL
-			else if(testIfSymbol(lineCandidate[j]))
+			else if(testIfSymbol(inputLine[i]))
 			{
-				pushSymbol(lineCandidate, j);
+				pushSymbol(inputLine, i);
+			}
+			// ASCII value of 'tab' is 9
+			else if(inputLine[i] == ' ' || inputLine[i] == 9)
+			{
+				i++;
 			}
 			else
 			{
 				error = true;
 			}
 
-			if (j == lineCandidate.size())
+			if (i >= inputLine.size())
 			{
-				i++;
-				j = 0;
+				getline(infile, inputLine);
+				i = 0;
 			}
+		}
+
+		for (int i = 0; i < _tokens.size(); i++)
+		{
+			cout << _tokens[i] << " : " << _lexemes[i] << endl;
 		}
 	}
 };
 
 int main()
 {
+	string line;
+	string test;
 	ifstream tLData("tokenlexemedata.txt");
 	ifstream sCode("sourceCode.txt");
 	ofstream oFile("outputfile.txt");
 	LexAnalyzer scanner(tLData);
 	scanner.scanFile(sCode, oFile);
-
 
 	return 0;
 }
